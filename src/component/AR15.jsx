f9b6d2cf34da1f42bef6d08a8cfec4e893319c9c
rwny <rawee6426@gmail.com>
2025-04-12 12:43:29 +0700

    1243

--- src/component/AR15.jsx
+++ src/component/AR15.jsx
@@ -1,17 +1,16 @@
 function AR15() {
    return(
       <>
-         <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]} scale={1}>
+         <mesh position={[0, 2.5, 0]} rotation={[Math.PI / 2, 0, 0]} scale={1}>
             <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial color="blue" />
          </mesh>
          <mesh>
             <torusKnotGeometry/>
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color="green" />
          </mesh>
       </>
 
    )
 }
-
 export default AR15;
